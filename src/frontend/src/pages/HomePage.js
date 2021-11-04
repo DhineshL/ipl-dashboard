4a7f6e9b1779ee72a9050885b32282e960d1daad
dhineshL <dhineshdl22@gmail.com>
2021-11-04 22:57:22 +0530

    Home Page of IPL Dashboard is done

---
+++ src/frontend/src/pages/HomePage.js
@@ -0,0 +1,30 @@
+import { React, useEffect, useState } from 'react'
+import { TeamTile } from '../components/TeamTile'
+
+import './HomePage.scss'
+
+export const HomePage = () => {
+  const [teams, setTeams] = useState([])
+  useEffect(() => {
+    const fetchAllTeams = async () => {
+      const response = await fetch(`http://localhost:8080/team`)
+      const data = await response.json()
+      console.log(data)
+      setTeams(data)
+    }
+    fetchAllTeams()
+  }, [])
+
+  return (
+    <div className='HomePage'>
+      <div className='header-section'>
+        <h1 className='app-name'>Java Brains IPL Dashboard</h1>
+      </div>
+      <div className='team-grid'>
+        {teams.map((team) => (
+          <TeamTile teamName={team.teamName}></TeamTile>
+        ))}
+      </div>
+    </div>
+  )
+}
