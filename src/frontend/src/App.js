a909a3bcac398966af3e0fb21941ff2249f072c8
dhineshL <dhineshdl22@gmail.com>
2021-11-04 22:56:38 +0530

    Home Page of IPL Dashboard is done

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { TeamPage } from './pages/TeamPage'
 import { MatchPage } from './pages/MatchPage'
+import { HomePage } from './pages/HomePage'
+
 function App() {
   return (
     <div className='App'>
@@ -12,6 +14,9 @@ function App() {
           </Route>
           <Route path='/teams/:teamName'>
             <TeamPage />
+          </Route>
+          <Route path='/'>
+            <HomePage />
           </Route>
         </Switch>
       </Router>
